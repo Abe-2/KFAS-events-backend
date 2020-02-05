bb4e4b23666f7d92d6f9fcea4c97ff625c8c5d28
abe-2 <agg2@outlook.com>
2020-02-05 23:46:15 +0300

    - test email

--- events/settings.py
+++ events/settings.py
@@ -148,12 +148,15 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # email
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp-mail.outlook.com'
+# EMAIL_HOST = 'smtp-mail.outlook.com'
 # EMAIL_HOST_USER = 'kfas-2@outlook.com'
 # EMAIL_HOST_PASSWORD = 'CodedKfas2'
+# EMAIL_PORT = 25
+
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'kfas1111111@gmail.com'
 EMAIL_HOST_PASSWORD = 'Coded-Kfas-1'
-EMAIL_PORT = 25
+EMAIL_PORT = 587
 
 TEMPLATES = [
     {
