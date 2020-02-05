ec138f9029ae54e7d0f50d6156791afdf744065a
abe-2 <agg2@outlook.com>
2020-02-05 22:16:30 +0300

    - email change

--- events/settings.py
+++ events/settings.py
@@ -149,8 +149,8 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # email
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp-mail.outlook.com'
-EMAIL_HOST_USER = 'kfas-1@outlook.com'
-EMAIL_HOST_PASSWORD = 'CodedKfas1'
+EMAIL_HOST_USER = 'kfas-2@outlook.com'
+EMAIL_HOST_PASSWORD = 'CodedKfas2'
 EMAIL_PORT = 25
 
 TEMPLATES = [
